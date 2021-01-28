bb1cbe65cfaf6582924914e679114c59ddd34cce
terintelivia <70704787+terintelivia@users.noreply.github.com>
2021-01-28 09:31:31 +0200

    Add files via upload

---
+++ pr independent.py
@@ -0,0 +1,12 @@
+prenume=['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+varsta=[14, 23, 15, 14, 12, 41, 39]
+print('Mihai are varsta de',14, 'ani')
+print('George are varsta de',23, 'ani')
+print('Ana are varsta de',15, 'ani')
+print('Dan are varsta de',14, 'ani')
+print('Ion are varsta de',12, 'ani')
+print('Geta are varsta de',41, 'ani')
+print('Vio are varsta de',39, 'ani')
+prenume.append('Andreea')
+print(prenume)
+varsta.append(34)
