2ad97c20516dee149f151e608785c3244f31e184
terintelivia <70704787+terintelivia@users.noreply.github.com>
2021-01-30 17:50:32 +0200

    Add files via upload

---
+++ problema lista.py
@@ -0,0 +1,36 @@
+prenume=['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+varsta=[14, 23, 15, 14, 12, 41, 39]
+print('Mihai are varsta de',14, 'ani')
+print('George are varsta de',23, 'ani')
+print('Ana are varsta de',15, 'ani')
+print('Dan are varsta de',14, 'ani')
+print('Ion are varsta de',12, 'ani')
+print('Geta are varsta de',41, 'ani')
+print('Vio are varsta de',39, 'ani')
+prenume.append('Andreea')
+print(prenume)
+varsta.append(34)
+print(varsta)
+prenume=['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+varsta=[14, 23, 15, 14, 12, 41, 39]
+prenume.remove('Ana')
+print(prenume)
+varsta.remove(15)
+print(varsta)
+prenume=['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+lista1=prenume[0:3]
+print(lista1)
+prenume=['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+print(prenume[::-1])
+prenume=['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+lista1=prenume[0:5]
+print(lista1)
+prenume=['Mihai','George','Ana','Dan','Ion','Geta','Vio']
+print(prenume[2])
+print(prenume[4])
+varsta=[14, 23, 15, 14, 12, 41, 39]
+lista1=varsta[0:5]
+print(lista1)
+varsta=[14, 23, 15, 14, 12, 41, 39]
+print(varsta[2])
+print(varsta[4])
